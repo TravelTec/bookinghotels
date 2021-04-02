898717d62b5dc4d9361f8ea7eb58ff42993deb4d
Raabe Sampaio <raabe@montenegroev.com.br>
2021-04-02 12:41:57 -0300

    v1.0.10 - Duplicidade hospedagens

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ GitHub Plugin URI: https://github.com/TravelTec/bookinghotels
 
 Description: Voucher Tec - Tarifário de hotéis é um plugin desenvolvido para agências e operadoras de turismo que precisam tratar diárias de hospedagem de várias propriedades simultaneamente.
 
-Version: 1.0.7
+Version: 1.0.10
 
 Author: Travel Tec
 
